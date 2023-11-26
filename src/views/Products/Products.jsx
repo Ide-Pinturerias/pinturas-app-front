410636dd900a5be61b1733f0dbdfd7dd6dad6cb8
SebastianMartel <martel.ssebastian@gmail.com>
2023-11-25 23:25:40 -0500

    restructuring products

--- src/views/Products/Products.jsx
+++ src/views/Products/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import FeaturedContainer from '@components/FeaturedContainer/FeaturedContainer'
 import { bestSellers } from '@redux/actions/Products/bestSellers'
 import ProductsContainer from '@components/ProductsContainer/ProductsContainer'
@@ -6,35 +6,67 @@ import { useDispatch, useSelector } from 'react-redux'
 import { allProducts } from '@redux/actions/Products/allProducts'
 import { allCategories } from '@redux/actions/Categories/allCategories'
 import { getProductFilter } from '@redux/actions/filters/getProductFilter'
+import { Chevron } from '../../components/SVG'
 
-const ProductsPage = () => {
-  const dispatch = useDispatch()
-  const filterCategory = useSelector((state) => state.filterCategory)
-  const thisPage = useSelector((state) => state.thisPage)
 
-  useEffect(() => {
-    dispatch(allProducts(thisPage))
-    if (!filterCategory) {
-      dispatch(allCategories())
-    } else {
-      dispatch(getProductFilter(thisPage, filterCategory))
-    }
-  }, [dispatch, thisPage, filterCategory])
+function ProductsPage() {
 
-  useEffect(() => {
-    dispatch(bestSellers())
-  }, [dispatch])
 
-  return (
-    <div>
-      <div>
-        <ProductsContainer />
-      </div>
-      <div>
-        <FeaturedContainer />
-      </div>
-    </div>
-  )
-}
+    // GLOBAL STATES:
+    const filterCategory = useSelector((state) => state.filterCategory);
+    const thisPage = useSelector((state) => state.thisPage);
 
-export default ProductsPage
+    const dispatch = useDispatch();
+
+
+    // LIFE CYCLES:
+    useEffect(() => {
+        dispatch(allProducts(thisPage));
+        if (!filterCategory) {
+            dispatch(allCategories());
+        } else {
+            dispatch(getProductFilter(thisPage, filterCategory));
+        };
+    }, [dispatch, thisPage, filterCategory]);
+
+    useEffect(() => {
+        dispatch(bestSellers());
+    }, [dispatch]);
+
+
+    // COMPONENT:
+    return (
+        <main className="relative p-whiteSpaceTop w-full before:content-[''] before:absolute before:-z-50 before:top-0 before:left-0 before:h-[100vh] before:min-w-full before:bg-primary">
+            <section className="mb-[8rem] text-white">
+                <div className="max-w-[1920px] px-[3.5%]">
+                    <h2 className="relative mt-[5rem] w-fit text-[clamp(.75vw,calc(7vw+.5rem),6rem)] font-bold before:content-none before:absolute before:-z-10 before:top-1/2 before:-translate-y-1/2 before:left-0 before:h-[125%] before:w-full before:bg-warmWhite before:rounded-[2rem]"> Todos nuestros productos </h2>
+                    <p className="mb-[5rem] text-[clamp(.5vw,calc(1.5vw+.3rem),2.5rem)]">
+                        Contamos con una gran seleccion de productos. Entre ellas destacamos las pinturas Fadema.
+                    </p>
+                    <button className="p-4 bg-white rounded-[1rem] text-black text-[clamp(.5vw,calc(1.5vw+.3rem),2.5rem)]">
+                        Filtrar
+                    </button>
+                    
+                </div>
+            </section>
+            <section className="mb-[8rem] text-white">
+                <div className="max-w-[1920px] px-[3.5%]">
+                    <h2 className="relative mt-[5rem] w-fit text-[clamp(.75vw,calc(7vw+.5rem),6rem)] font-bold before:content-none before:absolute before:-z-10 before:top-1/2 before:-translate-y-1/2 before:left-0 before:h-[125%] before:w-full before:bg-warmWhite before:rounded-[2rem]"> Todos nuestros productos </h2>
+                    <p className="mb-[5rem] text-[clamp(.5vw,calc(1.5vw+.3rem),2.5rem)]">
+                        Contamos con una gran seleccion de productos. Entre ellas destacamos las pinturas Fadema.
+                    </p>
+                </div>
+            </section>
+            {/* <div>
+                <ProductsContainer />
+            </div> */}
+
+            {/* <div>
+                <FeaturedContainer />
+            </div> */}
+        </main>
+    );
+};
+
+
+export default ProductsPage;

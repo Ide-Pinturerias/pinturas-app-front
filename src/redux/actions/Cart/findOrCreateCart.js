2fe312d34f2a94704834190f6c997df2f8530cbb
Edgar Juan Guercio <jguercio@live.com.ar>
2023-12-29 16:50:41 -0300

    FEAT set local storage en usuario nuevo con carro vacio

--- src/redux/actions/Cart/findOrCreateCart.js
+++ src/redux/actions/Cart/findOrCreateCart.js
@@ -4,9 +4,22 @@ import axios from 'axios'
 
 export const findOrCreateCart = (idUser) => {
   return async (dispatch) => {
-    const cart = (await axios.post(`${BASE_URL}carts`, { idUser })).data
-    // seteo localmente
-    localStorage.setItem('productsLocal', cart.products)
+    let cart = (await axios.post(`${BASE_URL}carts`, { idUser })).data
+
+    // si el carro tiene productos seteo localmente
+    if (cart.products !== null) {
+      localStorage.setItem('productsLocal', cart.products)
+    }
+
+    // en caso de tener productos localmente y luego crearme un usuario necesito guardarlos en la db y Redux
+    const productsLocal = JSON.parse(localStorage.getItem('productsLocal')) || []
+    if (cart.id === null & productsLocal.length > 0) {
+      cart = (await axios.put(`${BASE_URL}carts`, {
+        idUser,
+        products: JSON.stringify(productsLocal)
+      })).data
+    }
+
     // seteo estado global
     dispatch({ type: FIND_OR_CREATE_CART, payload: cart })
   }

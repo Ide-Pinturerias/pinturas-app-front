986c724c5c6730e827a3957c0997d859cc5d57ed
Edgar Juan Guercio <jguercio@live.com.ar>
2024-03-06 01:29:10 -0300

    DELETE estado global para favoritos duplicado

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,290 +1,289 @@
 import {
-  // PRODUCTS
-  GET_ALL_PRODUCTS_PAGINATED,
-  POST_PRODUCT,
-  PUT_PRODUCT,
-  GET_PRODUCT_BY_ID,
-  GET_PRODUCT_BY_NAME,
-  GET_ALL_PRODUCTS_NO_FILTER,
-  CLEAN_PRODUCT_DETAIL,
+    // PRODUCTS
+    GET_ALL_PRODUCTS_PAGINATED,
+    POST_PRODUCT,
+    PUT_PRODUCT,
+    GET_PRODUCT_BY_ID,
+    GET_PRODUCT_BY_NAME,
+    GET_ALL_PRODUCTS_NO_FILTER,
+    CLEAN_PRODUCT_DETAIL,
 
-  // CATEGORIES
-  GET_ALL_CATEGORIES,
+    // CATEGORIES
+    GET_ALL_CATEGORIES,
 
-  // USERS
-  POST_REGISTER_USER,
-  POST_LOGIN_USER,
-  GET_ALL_USERS,
-  DELETE_USER,
-  PUT_USER,
-  ACCESS_TOKEN,
-  SET_USER,
+    // USERS
+    POST_REGISTER_USER,
+    POST_LOGIN_USER,
+    GET_ALL_USERS,
+    DELETE_USER,
+    PUT_USER,
+    ACCESS_TOKEN,
+    SET_USER,
 
-  // FILTERS
-  GET_ALL_PRODUCTS_FILTERED,
-  GET_BEST_SELL,
-  SET_CATEGORY,
-  SET_LOW_PRICE,
-  SET_HIGH_PRICE,
+    // FILTERS
+    GET_ALL_PRODUCTS_FILTERED,
+    GET_BEST_SELL,
+    SET_CATEGORY,
+    SET_LOW_PRICE,
+    SET_HIGH_PRICE,
 
-  // SORTING
-  SET_SORT_CLAUSE,
-  SET_SORT_DIRECTION,
+    // SORTING
+    SET_SORT_CLAUSE,
+    SET_SORT_DIRECTION,
 
-  // PAGES
-  SET_TOTAL_PAGES,
-  SET_PAGE,
+    // PAGES
+    SET_TOTAL_PAGES,
+    SET_PAGE,
 
-  // CART
-  FIND_OR_CREATE_CART,
-  ADD_PRODUCT_CART,
-  DELETE_PRODUCT_CART,
-  SET_CART,
-  CLEAR_CART,
+    // CART
+    FIND_OR_CREATE_CART,
+    ADD_PRODUCT_CART,
+    DELETE_PRODUCT_CART,
+    SET_CART,
+    CLEAR_CART,
 
-  // ORDERS
-  GET_ALL_ORDERS,
-  GET_ORDERS_USER,
-  GET_ORDER_BY_ID,
-  POST_ORDER_PAYMENT,
-  POST_ORDER_CART,
-  PUT_ORDER,
+    // ORDERS
+    GET_ALL_ORDERS,
+    GET_ORDERS_USER,
+    GET_ORDER_BY_ID,
+    POST_ORDER_PAYMENT,
+    POST_ORDER_CART,
+    PUT_ORDER,
 
-  // NODE MAILER
-  POST_CONTACT_EMAIL,
-  POST_ORDER_EMAIL,
-  POST_REGISTER_EMAIL,
-  LOGOUT_USER,
+    // NODE MAILER
+    POST_CONTACT_EMAIL,
+    POST_ORDER_EMAIL,
+    POST_REGISTER_EMAIL,
+    LOGOUT_USER,
 
-  // AUTH0-USERS-INFO
-  SET_USER_DATA,
-  GET_USER_BY_ID,
+    // AUTH0-USERS-INFO
+    SET_USER_DATA,
+    GET_USER_BY_ID,
 
-  // BLOG
-  GET_POSTS,
-  GET_POST_BY_ID,
-  DELETE_POST,
-  PUT_POST,
-  POST_POST,
+    // BLOG
+    GET_POSTS,
+    GET_POST_BY_ID,
+    DELETE_POST,
+    PUT_POST,
+    POST_POST,
 
-  // FAVORITES
-  ADD_FAVORITE,
-  GET_FAVORITES,
-  DELETE_FAVORITES,
+    // FAVORITES
+    ADD_FAVORITE,
+    GET_FAVORITES,
+    DELETE_FAVORITES,
 
-  // REVIEWS
-  SAVE_REVIEW,
+    // REVIEWS
+    SAVE_REVIEW,
 
-  // PROVIDERS
-  GET_PROVIDERS,
-  GET_PROVIDERS_ACTIVE,
-  GET_PROVIDER_BY_ID,
-  CLEAN_PROVIDER
+    // PROVIDERS
+    GET_PROVIDERS,
+    GET_PROVIDERS_ACTIVE,
+    GET_PROVIDER_BY_ID,
+    CLEAN_PROVIDER
 
 } from '../action-type'
 
 const initialState = {
-  // CART
-  cart: {},
-  // ORDERS
-  allOrders: [],
-  ordersUser: {},
-  orderDetail: {},
+    // CART
+    cart: {},
+    // ORDERS
+    allOrders: [],
+    ordersUser: {},
+    orderDetail: {},
 
-  // PRODUCTS
-  allProductsPaginated: [],
-  detail: {},
-  allProducts: [],
-  bestSellers: [],
+    // PRODUCTS
+    allProductsPaginated: [],
+    detail: {},
+    allProducts: [],
+    bestSellers: [],
 
-  // CATEGORIES
-  categories: [],
+    // CATEGORIES
+    categories: [],
 
-  // USER
-  user: {},
-  allUsers: [],
-  token: '',
-  userId: {},
+    // USER
+    user: {},
+    allUsers: [],
+    token: '',
+    userId: {},
 
-  // FILTERS
-  filterCategory: '',
-  price: {
-    highPrice: 0,
-    lowPrice: 0
-  },
+    // FILTERS
+    filterCategory: '',
+    price: {
+        highPrice: 0,
+        lowPrice: 0
+    },
 
-  // SORTING
-  sortBy: '', // 'price' | 'rating' | 'stock' | 'name' | undefined, '' => 'idProduct'
-  orderBy: '', // 'asc' | 'desc'
+    // SORTING
+    sortBy: '', // 'price' | 'rating' | 'stock' | 'name' | undefined, '' => 'idProduct'
+    orderBy: '', // 'asc' | 'desc'
 
-  // PAGES
-  totalPages: 0,
-  thisPage: 1,
+    // PAGES
+    totalPages: 0,
+    thisPage: 1,
 
-  // MAIL
-  mail: {},
+    // MAIL
+    mail: {},
 
-  // AUTH0-USERS-INFO
-  userData: {},
+    // AUTH0-USERS-INFO
+    userData: {},
 
-  // REVIEWS
-  userReview: {},
+    // REVIEWS
+    userReview: {},
 
-  // BLOG
-  posts: [],
-  post: {},
+    // BLOG
+    posts: [],
+    post: {},
 
-  // FAVORITES
-  favorites: [],
-  allFavorites: [],
+    // FAVORITES
+    allFavorites: [],
 
-  // PROVIDERS
-  providers: [],
-  providersActive: [],
-  provider: {}
+    // PROVIDERS
+    providers: [],
+    providersActive: [],
+    provider: {}
 }
 
 const reducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    // PRODUCTS
-    case GET_ALL_PRODUCTS_PAGINATED:
-      return { ...state, allProductsPaginated: payload }
-    case POST_PRODUCT:
-      return { ...state, allProductsPaginated: payload }
-    case PUT_PRODUCT:
-      return { ...state, allProductsPaginated: payload }
-    case GET_PRODUCT_BY_ID:
-      return { ...state, detail: payload }
-    case GET_BEST_SELL:
-      return { ...state, bestSellers: payload }
-    case GET_PRODUCT_BY_NAME:
-      return { ...state, allProductsPaginated: payload }
-    case GET_ALL_PRODUCTS_NO_FILTER:
-      return { ...state, allProducts: payload }
-    case CLEAN_PRODUCT_DETAIL:
-      return { ...state, detail: {} }
+    switch (type) {
+        // PRODUCTS
+        case GET_ALL_PRODUCTS_PAGINATED:
+            return { ...state, allProductsPaginated: payload }
+        case POST_PRODUCT:
+            return { ...state, allProductsPaginated: payload }
+        case PUT_PRODUCT:
+            return { ...state, allProductsPaginated: payload }
+        case GET_PRODUCT_BY_ID:
+            return { ...state, detail: payload }
+        case GET_BEST_SELL:
+            return { ...state, bestSellers: payload }
+        case GET_PRODUCT_BY_NAME:
+            return { ...state, allProductsPaginated: payload }
+        case GET_ALL_PRODUCTS_NO_FILTER:
+            return { ...state, allProducts: payload }
+        case CLEAN_PRODUCT_DETAIL:
+            return { ...state, detail: {} }
 
-    // CATEGORIES
-    case GET_ALL_CATEGORIES:
-      return { ...state, categories: payload }
+        // CATEGORIES
+        case GET_ALL_CATEGORIES:
+            return { ...state, categories: payload }
 
-    // USER
-    case POST_REGISTER_USER:
-      return { ...state, user: payload }
-    case POST_LOGIN_USER:
-      return { ...state, user: payload }
-    case GET_ALL_USERS:
-      return { ...state, allUsers: payload }
-    case DELETE_USER:
-      return { ...state }
-    case PUT_USER:
-      return { ...state, user: payload }
-    case LOGOUT_USER:
-      return { ...state, user: payload }
-    case ACCESS_TOKEN:
-      return { ...state, token: payload }
-    case SET_USER:
-      return { ...state, user: payload }
-    case GET_USER_BY_ID:
-      return { ...state, userId: payload }
+        // USER
+        case POST_REGISTER_USER:
+            return { ...state, user: payload }
+        case POST_LOGIN_USER:
+            return { ...state, user: payload }
+        case GET_ALL_USERS:
+            return { ...state, allUsers: payload }
+        case DELETE_USER:
+            return { ...state }
+        case PUT_USER:
+            return { ...state, user: payload }
+        case LOGOUT_USER:
+            return { ...state, user: payload }
+        case ACCESS_TOKEN:
+            return { ...state, token: payload }
+        case SET_USER:
+            return { ...state, user: payload }
+        case GET_USER_BY_ID:
+            return { ...state, userId: payload }
 
-    // FILTERS
-    case GET_ALL_PRODUCTS_FILTERED:
-      return { ...state, allProductsPaginated: payload }
-    case SET_CATEGORY:
-      return { ...state, filterCategory: payload }
-    case SET_HIGH_PRICE:
-      return { ...state, price: { ...state.price, highPrice: payload } }
-    case SET_LOW_PRICE:
-      return { ...state, price: { ...state.price, lowPrice: payload } }
+        // FILTERS
+        case GET_ALL_PRODUCTS_FILTERED:
+            return { ...state, allProductsPaginated: payload }
+        case SET_CATEGORY:
+            return { ...state, filterCategory: payload }
+        case SET_HIGH_PRICE:
+            return { ...state, price: { ...state.price, highPrice: payload } }
+        case SET_LOW_PRICE:
+            return { ...state, price: { ...state.price, lowPrice: payload } }
 
-    // SORTING
-    case SET_SORT_CLAUSE:
-      return { ...state, sortBy: payload }
-    case SET_SORT_DIRECTION:
-      return { ...state, orderBy: payload }
+        // SORTING
+        case SET_SORT_CLAUSE:
+            return { ...state, sortBy: payload }
+        case SET_SORT_DIRECTION:
+            return { ...state, orderBy: payload }
 
-    // PAGES
-    case SET_TOTAL_PAGES:
-      return { ...state, totalPages: payload }
-    case SET_PAGE:
-      return { ...state, thisPage: payload }
+        // PAGES
+        case SET_TOTAL_PAGES:
+            return { ...state, totalPages: payload }
+        case SET_PAGE:
+            return { ...state, thisPage: payload }
 
-    // CART
-    case FIND_OR_CREATE_CART:
-      return { ...state, cart: payload }
-    case ADD_PRODUCT_CART:
-      return { ...state, cart: payload }
-    case DELETE_PRODUCT_CART:
-      return { ...state, cart: payload }
-    case SET_CART:
-      return { ...state, cart: payload }
-    case CLEAR_CART:
-      return { ...state, cart: payload }
+        // CART
+        case FIND_OR_CREATE_CART:
+            return { ...state, cart: payload }
+        case ADD_PRODUCT_CART:
+            return { ...state, cart: payload }
+        case DELETE_PRODUCT_CART:
+            return { ...state, cart: payload }
+        case SET_CART:
+            return { ...state, cart: payload }
+        case CLEAR_CART:
+            return { ...state, cart: payload }
 
-    // NODE MAILER
-    case POST_CONTACT_EMAIL:
-      return { ...state, mail: payload }
-    case POST_ORDER_EMAIL:
-      return { ...state, mail: payload }
-    case POST_REGISTER_EMAIL:
-      return { ...state, mail: payload }
+        // NODE MAILER
+        case POST_CONTACT_EMAIL:
+            return { ...state, mail: payload }
+        case POST_ORDER_EMAIL:
+            return { ...state, mail: payload }
+        case POST_REGISTER_EMAIL:
+            return { ...state, mail: payload }
 
-    // ORDERS
-    case GET_ALL_ORDERS:
-      return { ...state, allOrders: payload }
-    case POST_ORDER_PAYMENT:
-      return { ...state }
-    case GET_ORDERS_USER:
-      return { ...state, ordersUser: payload }
-    case GET_ORDER_BY_ID:
-      return { ...state, orderDetail: payload }
-    case PUT_ORDER:
-      return { ...state, orderDetail: payload }
-    case POST_ORDER_CART:
-      return { ...state }
+        // ORDERS
+        case GET_ALL_ORDERS:
+            return { ...state, allOrders: payload }
+        case POST_ORDER_PAYMENT:
+            return { ...state }
+        case GET_ORDERS_USER:
+            return { ...state, ordersUser: payload }
+        case GET_ORDER_BY_ID:
+            return { ...state, orderDetail: payload }
+        case PUT_ORDER:
+            return { ...state, orderDetail: payload }
+        case POST_ORDER_CART:
+            return { ...state }
 
-    // AUTH0-USERS-INFO
-    case SET_USER_DATA:
-      return { ...state, userData: payload }
+        // AUTH0-USERS-INFO
+        case SET_USER_DATA:
+            return { ...state, userData: payload }
 
-    // BLOG
-    case GET_POSTS:
-      return { ...state, posts: payload }
-    case GET_POST_BY_ID:
-      return { ...state, post: payload }
-    case PUT_POST:
-      return { ...state }
-    case DELETE_POST:
-      return { ...state }
-    case POST_POST:
-      return { ...state }
+        // BLOG
+        case GET_POSTS:
+            return { ...state, posts: payload }
+        case GET_POST_BY_ID:
+            return { ...state, post: payload }
+        case PUT_POST:
+            return { ...state }
+        case DELETE_POST:
+            return { ...state }
+        case POST_POST:
+            return { ...state }
 
-    // REVIEWS
-    case SAVE_REVIEW:
-      return { ...state, userReview: payload }
+        // REVIEWS
+        case SAVE_REVIEW:
+            return { ...state, userReview: payload }
 
-    // FAVORITES
-    case ADD_FAVORITE:
-      return { ...state, favorites: payload }
-    case GET_FAVORITES:
-      return { ...state, allFavorites: payload }
-    case DELETE_FAVORITES:
-      return { ...state, allFavorites: payload }
+        // FAVORITES
+        case ADD_FAVORITE:
+            return { ...state, allFavorites: payload }
+        case GET_FAVORITES:
+            return { ...state, allFavorites: payload }
+        case DELETE_FAVORITES:
+            return { ...state, allFavorites: payload }
 
-    // PROVIDERS
-    case GET_PROVIDERS:
-      return { ...state, providers: payload }
-    case GET_PROVIDERS_ACTIVE:
-      return { ...state, providers: payload }
-    case GET_PROVIDER_BY_ID:
-      return { ...state, provider: payload }
-    case CLEAN_PROVIDER:
-      return { ...state, provider: payload }
+        // PROVIDERS
+        case GET_PROVIDERS:
+            return { ...state, providers: payload }
+        case GET_PROVIDERS_ACTIVE:
+            return { ...state, providers: payload }
+        case GET_PROVIDER_BY_ID:
+            return { ...state, provider: payload }
+        case CLEAN_PROVIDER:
+            return { ...state, provider: payload }
 
-    default:
-      return { ...state }
-  }
+        default:
+            return { ...state }
+    }
 }
 
 export default reducer

d562c9901e83018da56d7b8d0399690a93f7a57a
SebastianMartel <martel.ssebastian@gmail.com>
2024-03-14 22:04:12 -0500

    [FEAT]: Accordion components logic.

--- src/components/Cart/Accordion.jsx
+++ src/components/Cart/Accordion.jsx
@@ -1,8 +1,13 @@
+import { useState, Children, isValidElement, cloneElement } from "react"
 import { CaretDown } from "@svg"
 
-function AccordionButton({ children }) {
+function AccordionButton ({ setIsOpen, isOpen, children }) {
+    // isOpen state will be used for styles.
     return (
-        <button className="flex items-center justify-between w-full">
+        <button
+            className="flex items-center justify-between w-full"
+            onClick={setIsOpen}
+        >
             <div className="flex items-center gap-[8px]">
                 {children}
             </div>
@@ -11,18 +16,38 @@ function AccordionButton({ children }) {
     )
 }
 
-function AccordionContent({ children }) {
+function AccordionContent({ isOpen, children }) {
+    // isOpen state will be used for styles.
     return (
         <div className="border-t border-duller mt-[16px] pt-[16px]">
-            {children}z
+            {children}
         </div>
     )
 }
 
 function AccordionItem({ children }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleVisibility = () => {
+        setIsOpen((prev) => !prev)
+    }
+
     return (
         <li className="bg-bgFocus p-[16px] rounded-[10px] mb-[16px]">
-            {children}
+            {
+                Children.map(children, (child) => {
+                    if (isValidElement(child)) {
+                        if (child.type === AccordionButton) {
+                            return cloneElement(child, { setIsOpen: toggleVisibility, isOpen })
+                        }
+                        if (child.type === AccordionContent) {
+                            return cloneElement(child, { isOpen })
+                        }
+                    }
+                    return children
+                })
+            }
         </li>
     )
 }

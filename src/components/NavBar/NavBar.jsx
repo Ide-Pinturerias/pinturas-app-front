e190968b26ae384a7245bbf3132776ea7a8f6b1f
Edgar Guercio <eguercio.training@innevo.cl>
2024-04-25 20:26:15 -0300

    EDIT navbar responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '@img/logo-black.png';
-import { Cart, UserIcon } from '@svg';
+import { Cart, UserIcon, MenuIcon } from '@svg';
 
 function NavBar() {
-    const [credentialsMenu, setCredentialsMenu] = useState(false);
-    const credentialsMenuRef = useRef(null);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const location = useLocation();
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
     const handleOutsideClick = (event) => {
-        if (credentialsMenu && credentialsMenuRef.current && !credentialsMenuRef.current.contains(event.target)) {
-            setCredentialsMenu(false);
-        };
+        if (isMenuOpen && !event.target.closest('#navbar')) {
+            setIsMenuOpen(false);
+        }
     };
 
     useEffect(() => {
@@ -18,32 +19,37 @@ function NavBar() {
         return () => {
             document.removeEventListener('click', handleOutsideClick);
         }
-    }, [credentialsMenu]);
+    }, [isMenuOpen]);
 
-    const linkStl = 'relative flex items-center gap-2 py-[.5rem] px-[1em] bg-transparent rounded-[8px] capitalize cursor-pointer transition-colors hover:text-primaryVisible hover:fill-primaryVisible hover:bg-primaryClear';
+    const linkStl = 'relative flex items-center gap-2 py-2 px-4 bg-transparent rounded capitalize cursor-pointer hover:bg-primaryClear';
+    const activeLinkStl = 'bg-primaryVisible'; // estado activo
 
     return (
-        <header className={`z-50 fixed w-full top-0 left-0 px-4 py-4 h-auto bg-bgFocus bg-opacity-75 border font-secondary backdrop-blur-md transition-all duration-300 ease-in-out`}>
+        <header id="navbar" className="fixed w-full top-0 left-0 px-4 py-4 bg-bgFocus bg-opacity-75 backdrop-blur-md z-50">
             <div className="container mx-auto flex justify-between items-center">
-                {/* LEFT NAV LINKS */}
-                <nav className="flex items-center space-x-4">
-                    <Link to="/about" className={linkStl}>Nosotros</Link>
-                    <Link to="/contact" className={linkStl}>Contacto</Link>
-                    <Link to="/blog" className={linkStl}>Blog</Link>
-                </nav>
-
-                {/* CENTER LOGO */}
-                <div>
+                <button onClick={toggleMenu} className="text-2xl md:hidden mr-2">
+                    <MenuIcon />
+                </button>
+                <div className="flex-grow flex justify-center md:justify-start">
                     <Link to="/" className="shrink-0">
                         <img src={logo} alt="Logo" className="w-16" />
                     </Link>
                 </div>
-
-                {/* RIGHT NAV LINKS */}
-                <nav className="flex items-center space-x-4">
-                    <Link to="/products" className={linkStl}>Productos</Link>
-                    <Link to="/cart" className={linkStl}><Cart size={'1rem'} /> Carrito</Link>
-                    <Link to="/account" className={linkStl}><UserIcon size={'0.75rem'} /> Cuenta</Link>
+                <nav className={`${isMenuOpen ? 'block' : 'hidden'} md:block absolute md:relative top-full left-0 w-full md:w-auto bg-white md:bg-transparent z-20`}>
+                    <ul className="flex flex-col md:flex-row md:space-x-4 items-center text-center">
+                        {['/products', '/blog', '/about', '/contact', '/cart', '/account'].map((path, index) => {
+                            const isActive = location.pathname === path;
+                            return (
+                                <li key={index}>
+                                    <Link to={path} className={`${linkStl} ${isActive ? activeLinkStl : ''}`} onClick={() => setIsMenuOpen(false)}>
+                                        {path === '/cart' ? <><Cart size={'1rem'} /> Carrito</> : null}
+                                        {path === '/account' ? <><UserIcon size={'0.75rem'} /> Cuenta</> : null}
+                                        {['/products', '/blog', '/about', '/contact'].includes(path) ? path.substring(1) : null}
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </nav>
             </div>
         </header>

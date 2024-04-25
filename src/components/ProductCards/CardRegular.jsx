d9185997727d5d3c9ce0cd25c8551e02a6de2c72
Edgar Guercio <eguercio.training@innevo.cl>
2024-04-24 23:34:23 -0300

    DELETE buttons card without funcionality

--- src/components/ProductCards/CardRegular.jsx
+++ src/components/ProductCards/CardRegular.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-
 import { formatNumberWithDots } from '@/scripts/formatNumberWithDots'
-
-import { BookmarkOutlined } from '@mui/icons-material'
-import { Button } from '@components/Controls/Buttons';
 import { afterPseudo } from '@styles';
 
 
@@ -13,18 +9,12 @@ function CardRegular ({ id, name, category, color, image, brand, price }) {
   const [isFav, setIsFav] = useState(false)
 
   // STYLES:
-  const buttonFav = 'transition-all ease-linear duration-100 hover:stroke-primaryLight active:text-primaryDark active:scale-[.97]'
   const tag = 'w-fit h-fit flex items-center justify-center box-border px-[.5em] py-[.18em] border-[1.5px] rounded-[15px] border-primaryClear text-[.65rem] text-primaryClear whitespace-nowrap'
 
   // FUNCTIONS:
   const preventRedirection = (event) => {
     event.preventDefault()
     event.stopPropagation()
-  }
-
-  const addToFavourites = (event) => {
-    preventRedirection(event)
-    setIsFav((prev) => !prev)
   }
 
   // COMPONENT:
@@ -55,23 +45,11 @@ function CardRegular ({ id, name, category, color, image, brand, price }) {
                 {/* NAME & BOOKMARK */}
                 <div className='text-xl flex justify-between gap-2'>
                     <p className="m-0 white">{name}</p>
-                    <button
-                        className='self-start rounded-sm focus:outline focus:outline-focus focus:-outline-offset-[.5px]'
-                        onClick={addToFavourites}
-                    >
-                        <div className={`rounded-sm ${buttonFav} ${isFav ? 'text-primaryClear stroke-primaryClear' : 'text-bg stroke-clear'}`}>
-                            <BookmarkOutlined style={{ width: '1.7rem', height: '1.7rem' }} />
-                        </div>
-                    </button>
                 </div>
                 {/* BRAND */}
                 <p className='font-bold'>{brand}</p>
                 {/* PRICE */}
                 <strong className="text-[clamp(.5rem,calc(.75rem+1vw),3rem)]">$ {formatNumberWithDots(price)}</strong>
-            </div>
-            {/* ADD TO CART */}
-            <div className="flex justify-center w-full px-4 pb-4" onClick={preventRedirection}>
-                <Button variant="primary">Agregar al carro</Button>
             </div>
         </Link >
   )

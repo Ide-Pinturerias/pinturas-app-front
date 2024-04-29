c4a2a5f76cc1b1a5a81fe639f84e05c06b93a60d
SebastianMartel <martel.ssebastian@gmail.com>
2024-04-29 00:39:59 -0500

    [FEAT]: Hone -> About responsive.

--- src/components/AboutCard/AboutCard.jsx
+++ src/components/AboutCard/AboutCard.jsx
@@ -5,20 +5,20 @@ import { ButtonLink } from '@components/Controls/Links'
 const AboutCard = () => {
     return (
         <section className="flex items-center justify-center w-full mt-[50px] bg-bg">
-            <div className='flex justify-center items-center gap-[50px] m-sides max-w-maxSc w-maxIn'>
+            <div className='flex flex-col-reverse lg:flex-row justify-center items-center gap-[25px] md:gap-[50px] m-sides max-w-maxSc w-maxIn'>
                 <img
                     src={image1}
                     alt="Vista exterior de la tienda idePinturerias mostrando productos de pintura y herramientas."
-                    className='self-center items-center object-cover aspect-[4/3] w-[calc((100%-50px)/2)] rounded-lg'
+                    className='self-center items-center object-cover aspect-[4/3] w-full max-h-[400px] lg:w-[calc((100%-50px)/2)] rounded-lg'
                 />
-                <div className="flex flex-col justify-center w-[calc((100%-50px)/2)] mt-3">
-                    <h2 className="text-clear text-[clamp(1rem,calc(0.75rem+5vw),4rem)] text-start font-heading font-bold">
+                <div className="flex flex-col items-start lg:items-center w-full lg:w-[calc((100%-50px)/2)] mt-3">
+                    <h2 className="self-start text-clear text-[clamp(1rem,calc(0.75rem+5vw),4rem)] text-start font-heading font-bold">
                         Conózcanos
                     </h2>
-                    <p className="text-clear text-md mb-3 md:mb-11 text-start">
+                    <p className="text-clear text-md mb-3 md:mb-6    text-start">
                         Dedicados y comprometidos al 100% con pasión por la calidad, en idePinturerias priorizamos la excelencia y servicio personalizado. Nos esforzamos por superar tus expectativas al ofrecerte un servicio excepcional en pinturas y ferretería. Queremos ser tu socio confiable mientras nuestro equipo te brinda asesoramiento en cada proyecto, ya sea grande o pequeño.
                     </p>
-                    <ButtonLink path='/about' styles={{ 'alignSelf': 'center' }}>
+                    <ButtonLink path='/about'>
                         Nuestra historia
                     </ButtonLink>
                 </div>

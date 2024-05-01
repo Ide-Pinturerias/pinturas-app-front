e30aa24669b7d12c29d76383619f63524118932d
Edgar Guercio <eguercio.training@innevo.cl>
2024-05-01 15:38:46 -0300

    EDIT about and contact views styles

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   // Textos predefinidos
   const PARRAGRAPH_1 = 'Nuestra historia comenzó como un emprendimiento familiar, impulsado por la visión y pasión de Rodolfo Miguel Guercio. Inicialmente ubicada en la pintoresca localidad de Alta Gracia – Córdoba, pronto nos dimos cuenta de la creciente demanda de pinturas de alta calidad para obras industriales y proyectos particulares en la próspera Villa Anisacate.'
   const PARRAGRAPH_2 = 'Con astucia y determinación, nos trasladamos estratégicamente a Villa Anisacate, donde encontramos nuestro hogar en una localidad en pleno desarrollo y expansión. Desde entonces, hemos trabajado incansablemente para consolidarnos como líderes en la industria, ofreciendo una amplia gama de pinturas FADEPA, accesorios para pintores y herramientas de ferretería de las principales marcas.'
-  const PARRAGRAPH_3 = 'En ide Pinturerias, creemos en la excelencia y el servicio personalizado. Nuestro equipo dedicado está aquí para asesorarte en cada paso de tu proyecto, desde grandes obras hasta pequeños proyectos de decoración. Nuestra pasión por la calidad y la satisfacción del cliente nos impulsa a superar las expectativas y a ser tu socio confiable en todas tus necesidades relacionadas con pinturas y ferretería.'
+  const PARRAGRAPH_3 = 'En IDE Pinturerías, creemos en la excelencia y el servicio personalizado. Nuestro equipo dedicado está aquí para asesorarte en cada paso de tu proyecto, desde grandes obras hasta pequeños proyectos de decoración. Nuestra pasión por la calidad y la satisfacción del cliente nos impulsa a superar las expectativas y a ser tu socio confiable en todas tus necesidades relacionadas con pinturas y ferretería.'
 
   return (
     <main className="flex flex-col items-center mt-20 px-4 md:px-6 lg:px-8">
@@ -45,18 +45,18 @@ const About = () => {
       <div className="flex flex-col lg:flex-row justify-between items-center gap-7 w-full max-w-4xl mx-auto mb-11 p-5 rounded shadow-sm">
         <img src={imgAbout2} alt="img2" className="h-32 w-32 rounded-full" />
         <div className="flex-1">
-          <p className="text-blue-600 font-bold">Cómo trabajamos en ide Pinturerias</p>
+          <p className="text-blue-600 font-bold">Cómo trabajamos en ide Pinturerías</p>
           <p className="text-gray-600 text-sm mb-2">
-            Unión: Fomentamos un ambiente de trabajo basado en el respeto y apoyo mutuo, creando una familia empresarial que se preocupa por el bienestar de cada miembro.
+            <strong>Unión:</strong> Fomentamos un ambiente de trabajo basado en el respeto y apoyo mutuo, creando una familia empresarial que se preocupa por el bienestar de cada miembro.
           </p>
           <p className="text-gray-600 text-sm mb-2">
-            Compromiso: Nos comprometemos a brindar un servicio excepcional y a cumplir con las expectativas de nuestros clientes, buscando siempre superar sus necesidades y deseos.
+            <strong>Compromiso:</strong> Nos comprometemos a brindar un servicio excepcional y a cumplir con las expectativas de nuestros clientes, buscando siempre superar sus necesidades y deseos.
           </p>
           <p className="text-gray-600 text-sm mb-2">
-            Superación: Buscamos el crecimiento constante y la mejora continua en todo lo que hacemos, tanto a nivel individual como colectivo, para alcanzar el máximo potencial.
+            <strong>Superación:</strong> Buscamos el crecimiento constante y la mejora continua en todo lo que hacemos, tanto a nivel individual como colectivo, para alcanzar el máximo potencial.
           </p>
           <p className="text-gray-600 text-sm mb-2">
-            Desarrollo personal: Valoramos y promovemos el desarrollo profesional y personal de nuestro equipo, brindándoles oportunidades de crecimiento y capacitación para alcanzar sus metas y sueño.
+            <strong>Desarrollo personal:</strong> Valoramos y promovemos el desarrollo profesional y personal de nuestro equipo, brindándoles oportunidades de crecimiento y capacitación para alcanzar sus metas y sueño.
           </p>
         </div>
       </div>
--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -5,14 +5,14 @@ import Paws from "@img/paws.png";
 
 const Contact = () => {
   return (
-    <main className="relative z-10 flex flex-col items-center p-whiteSpaceTop w-full before:content-[''] before:absolute before:-z-50 before:top-0 before:left-0 before:h-[100vh] before:min-w-full before:bg-accentClear">
+    <main className="relative z-10 flex flex-col items-center p-whiteSpaceTop w-full before:content-[''] before:absolute before:-z-50 before:top-0 before:left-0 before:h-[100vh] before:min-w-full before:bg-fadepa">
       <section className="flex justify-center mb-16 text-white w-full">
         <div className="max-w-7xl w-full px-4 sm:px-6 lg:px-8">
           <img src={Local} alt="Local" className="shadow-lg w-full rounded-2xl" />
         </div>
       </section>
 
-      <section className="flex justify-center py-16 w-full bg-fadepa text-white">
+      <section className="flex justify-center py-16 w-full bg-accentClear text-white">
         <div className="px-4 sm:px-6 lg:px-8 w-full max-w-7xl">
           <div className="flex flex-col lg:flex-row justify-between items-center mb-16">
             <img

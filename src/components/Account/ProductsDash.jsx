287b41854b861f2cd771d354a53454f00d906026
Edgar Guercio <eguercio.training@innevo.cl>
2024-05-04 18:56:11 -0300

    USE ButtonLinks reutilizable

--- src/components/Account/ProductsDash.jsx
+++ src/components/Account/ProductsDash.jsx
@@ -1,122 +1,114 @@
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import UpdateButton from '../UpdateButton/UpdateButton.jsx'
-import DeleteButton from '../DeleteButton/DeleteButton.jsx'
-import { useDispatch, useSelector } from 'react-redux'
-import { getAllProductsNoFilter } from '@redux/actions/Products/getAllProductsNoFilter.js'
-import { DataGrid, GridToolbar } from '@mui/x-data-grid'
-import { ButtonLink } from '../Controls/Links.jsx'
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import UpdateButton from "../UpdateButton/UpdateButton.jsx";
+import DeleteButton from "../DeleteButton/DeleteButton.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProductsNoFilter } from "@redux/actions/Products/getAllProductsNoFilter.js";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { ButtonLink } from "../Controls/Links.jsx";
+import { useMediaQuery } from "@mui/material";
 
 const ProductsDash = () => {
-  const dispatch = useDispatch()
-  const products = useSelector((state) => state.allProducts)
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.allProducts);
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
   useEffect(() => {
-    getAllProductsNoFilter()(dispatch)
-  }, [dispatch])
+    getAllProductsNoFilter()(dispatch);
+  }, [dispatch]);
 
-  const columns = [
+  const columnsForMobile = [
+    { field: "id", headerName: "ID", width: 40 },
+    { field: "name", headerName: "Nombre", width: 200 },
     {
-      field: 'id',
-      headerName: 'ID',
-      flex: 0.2,
-      minWidth: 40
+      field: "edit",
+      headerName: "Editar",
+      renderCell: (params) => <UpdateButton idProduct={params.row.id} />,
+      width: 90,
     },
     {
-      field: 'name',
-      headerName: 'Nombre',
-      flex: 2,
-      minWidth: 120
+      field: "delete",
+      headerName: "Eliminar",
+      renderCell: (params) => <DeleteButton idProduct={params.row.id} />,
+      width: 90,
     },
-    {
-      field: 'category',
-      headerName: 'Categoría',
-      flex: 3,
-      minWidth: 180
-    },
-    {
-      field: 'package',
-      headerName: 'Formato',
-      flex: 0.8,
-      minWidth: 80
-    },
-    {
-      field: 'stock',
-      headerName: 'Stock',
-      flex: 0.9,
-      minWidth: 60
-    },
-    {
-      field: 'edit',
-      headerName: 'Editar',
-      renderCell: (params) => (<UpdateButton idProduct={params.row.id} />),
-      flex: 4,
-      minWidth: 200
-    },
-    {
-      field: 'delete',
-      headerName: 'Eliminar',
-      renderCell: (params) => (<DeleteButton idProduct={params.row.id} />),
-      flex: 4,
-      minWidth: 200
-    }
+  ];
 
-  ]
+  const columnsForDesktop = [
+    { field: "id", headerName: "ID", width: 40 },
+    { field: "name", headerName: "Nombre", width: 300 },
+    { field: "category", headerName: "Categoría", width: 200 },
+    { field: "package", headerName: "Formato", width: 80 },
+    { field: "stock", headerName: "Stock", width: 60 },
+    {
+      field: "edit",
+      headerName: "Editar",
+      renderCell: (params) => <UpdateButton idProduct={params.row.id} />,
+      width: 200,
+    },
+    {
+      field: "delete",
+      headerName: "Eliminar",
+      renderCell: (params) => <DeleteButton idProduct={params.row.id} />,
+      width: 200,
+    },
+  ];
 
   return (
-        <div className="container mx-auto px-4 mt-16">
-                <section className='p-8'>
-                  <ButtonLink path="/admin/create">Crear Producto</ButtonLink>
-                  <ButtonLink path="/admin/products/update/prices">Actualizar Precios</ButtonLink>
-                </section>
-            <div className="flex">
-                <DataGrid
-                    rows={products.map(product => ({
-                      id: product.idProduct,
-                      name: product.name,
-                      category: product.category,
-                      package: product.package,
-                      stock: product.stock
-                    }))}
-                    columns={columns}
-                    initialState={{
-                      pagination: {
-                        paginationModel: {
-                          page: 0,
-                          pageSize: 15
-                        }
-                      },
-                      sorting: true,
-                      filter: {
-                        filterModel: {
-                          items: []
-                        }
-                      }
+    <div className="container mx-auto px-4 mt-16">
+      <section className="p-8">
+        <ButtonLink path="/admin/create">Crear Producto</ButtonLink>
+        <ButtonLink path="/admin/products/update/prices">
+          Actualizar Precios
+        </ButtonLink>
+      </section>
+      <div className="flex">
+        <DataGrid
+          rows={products.map((product) => ({
+            id: product.idProduct,
+            name: product.name,
+            category: product.category,
+            package: product.package,
+            stock: product.stock,
+          }))}
+          columns={isMobile ? columnsForMobile : columnsForDesktop}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                page: 0,
+                pageSize: 15,
+              },
+            },
+            sorting: true,
+            filter: {
+              filterModel: {
+                items: [],
+              },
+            },
+          }}
+          slots={{ toolbar: GridToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+            },
+          }}
+          disableColumnFilter
+          pageSizeOptions={[5, 10, 15, 20, 50, 100]}
+        />
+      </div>
 
-                    }}
-                    slots={{ toolbar: GridToolbar }}
-                    slotProps={{
-                      toolbar: {
-                        showQuickFilter: true
-                      }
-                    }}
-                    disableColumnFilter
-                    pageSizeOptions={[5, 10, 15, 20, 50, 100]}
-                />
+      <div className="flex justify-between m-10">
+        <Link to="/products">
+          <button
+            type="submit"
+            className="bg-fadepa hover:bg-primaryClear text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Ir a buscar Productos
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-            </div>
-
-            <div className="flex justify-between m-10">
-                <Link to="/products">
-                    <button
-                        type="submit"
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        Ir a buscar Productos
-                    </button>
-                </Link>
-            </div>
-        </div>
-  )
-}
-
-export default ProductsDash
+export default ProductsDash;

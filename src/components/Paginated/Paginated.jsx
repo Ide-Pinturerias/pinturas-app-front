45a69ff43dc67357842376c8cc3e17f583eb7d25
Edgar Guercio <eguercio.training@innevo.cl>
2024-06-15 13:56:44 -0300

    HOTFIX pagination hover sitll activate on newt forward buttons

--- src/components/Paginated/Paginated.jsx
+++ src/components/Paginated/Paginated.jsx
@@ -1,85 +1,111 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import { afterPseudo } from '@styles';
 
 const Paginated = ({ thisPage, totalPages, pageChange }) => {
     const [pages, setPages] = useState([]);
 
-    const buttonClass = `relative bg-transparent after:w-full after:h-full after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2`
-    const currentButtonClass = 'bg-primaryClear text-primaryVisible'
-    const buttonBaseClass = 'size-[3rem] mx-[.25rem] rounded-lg font-bold'
+    const buttonClass = `hover:bg-gray-200 relative bg-transparent after:w-full after:h-full after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2`;
+    const currentButtonClass = 'bg-primaryClear text-primaryVisible';
+    const buttonBaseClass = 'size-[3rem] mx-[.25rem] rounded-lg font-bold';
 
     useEffect(() => {
         const renderPageNumbers = () => {
             const pagesArray = [];
             const startPage = Math.max(1, thisPage - 1);
             const endPage = Math.min(totalPages, thisPage + 1);
-    
+
             if (startPage > 1) {
                 pagesArray.push(
                     <li key={1}>
-                        <button className={`${buttonBaseClass} ${thisPage === 1 ? currentButtonClass : `${afterPseudo} ${buttonClass}`}`} onClick={() => pageChange(1)} disabled={thisPage === 1}>
-                            {1}
+                        <button
+                            className={`${buttonBaseClass} ${thisPage === 1 ? currentButtonClass : `${afterPseudo} ${buttonClass}`}`}
+                            onClick={() => pageChange(1)}
+                            disabled={thisPage === 1}
+                        >
+                            1
                         </button>
                     </li>
                 );
                 if (startPage > 2) {
-                    pagesArray.push(<li key="ellipsis-start"><div className="flex items-center justify-center h-[3rem] w-auto mx-[.25rem]"><span className="mb-[.45em]">...</span></div></li>);
+                    pagesArray.push(
+                        <li key="ellipsis-start">
+                            <div className="flex items-center justify-center h-[3rem] w-auto mx-[.25rem]">
+                                <span className="mb-[.45em]">...</span>
+                            </div>
+                        </li>
+                    );
                 }
             }
-            for (let i = startPage, len = endPage; i <= len; i++) {
+
+            for (let i = startPage; i <= endPage; i++) {
                 pagesArray.push(
                     <li key={i}>
-                        <button className={`${buttonBaseClass} ${thisPage === i ? currentButtonClass : `${afterPseudo} ${buttonClass}`}`} onClick={() => pageChange(i)} disabled={thisPage === i}>
+                        <button
+                            className={`${buttonBaseClass} ${thisPage === i ? currentButtonClass : `${afterPseudo} ${buttonClass}`}`}
+                            onClick={() => pageChange(i)}
+                            disabled={thisPage === i}
+                        >
                             {i}
                         </button>
                     </li>
                 );
             }
+
             if (endPage < totalPages) {
                 if (endPage < totalPages - 1) {
-                    pagesArray.push(<li key="ellipsis-end"><div className="flex items-center justify-center h-[3rem] w-auto mx-[.25rem]"><span className="mb-[.45em]">...</span></div></li>);
+                    pagesArray.push(
+                        <li key="ellipsis-end">
+                            <div className="flex items-center justify-center h-[3rem] w-auto mx-[.25rem]">
+                                <span className="mb-[.45em]">...</span>
+                            </div>
+                        </li>
+                    );
                 }
                 pagesArray.push(
                     <li key={totalPages}>
-                        <button className={`${buttonBaseClass} ${thisPage === totalPages ? currentButtonClass : `${afterPseudo} ${buttonClass}`}`} onClick={() => pageChange(totalPages)} disabled={thisPage === totalPages}>
+                        <button
+                            className={`${buttonBaseClass} ${thisPage === totalPages ? currentButtonClass : `${afterPseudo} ${buttonClass}`}`}
+                            onClick={() => pageChange(totalPages)}
+                            disabled={thisPage === totalPages}
+                        >
                             {totalPages}
-                        </button>    
+                        </button>
                     </li>
                 );
             }
             return pagesArray;
         };
 
-        setPages(renderPageNumbers())
-    }, [thisPage, totalPages])
+        setPages(renderPageNumbers());
+    }, [thisPage, totalPages]);
 
-  return (
-    <div className="flex items-center justify-center my-11">
-      <nav aria-label="Page navigation example">
-        <ol className="flex items-center text-base">
-          <li>
-            <button
-              className={`${buttonBaseClass} ${thisPage === 1 ? currentButtonClass : `${afterPseudo} ${buttonClass}`}`}
-              onClick={() => pageChange(thisPage - 1)}
-              disabled={thisPage === 1}
-            >
-              &lt;
-            </button>
-          </li>
-            {pages}
-          <li>
-            <button 
-              className={`${buttonBaseClass} ${thisPage === totalPages ? currentButtonClass : `${afterPseudo} ${buttonClass}`}`}
-              onClick={() => pageChange(thisPage + 1)}
-              disabled={thisPage === totalPages}
-            >
-              &gt;
-            </button>
-          </li>
-        </ol>
-      </nav>
-    </div>
-  )
-}
+    return (
+        <div className="flex items-center justify-center my-11">
+            <nav aria-label="Page navigation example">
+                <ol className="flex items-center text-base">
+                    <li>
+                        <button
+                            className={`${buttonBaseClass} ${thisPage === 1 ? currentButtonClass : ` ${buttonClass}`}`}
+                            onClick={() => pageChange(thisPage - 1)}
+                            disabled={thisPage === 1}
+                        >
+                            &lt;
+                        </button>
+                    </li>
+                    {pages}
+                    <li>
+                        <button
+                            className={`${buttonBaseClass} ${thisPage === totalPages ? currentButtonClass : `${buttonClass}`}`}
+                            onClick={() => pageChange(thisPage + 1)}
+                            disabled={thisPage === totalPages}
+                        >
+                            &gt;
+                        </button>
+                    </li>
+                </ol>
+            </nav>
+        </div>
+    );
+};
 
-export default Paginated
+export default Paginated;
